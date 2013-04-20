c1aef452017ee7630dbfae041fefa148bc6bd332
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2013-04-20 20:12:04 +0530

    Changed the date format on index page and posts

--- conf.py
+++ conf.py
@@ -250,7 +250,7 @@ THEME = 'custom'
 # THEME_REVEAL_CONGIF_TRANSITION = 'cube' # You can also use: page/concave/linear/none/default
 
 # date format used to display post dates. (str used by datetime.datetime.strftime)
-# DATE_FORMAT = '%Y-%m-%d %H:%M'
+DATE_FORMAT = '%a, %d %b %Y %H:%M:%S %z'
 
 # FAVICONS contains (name, file, size) tuples.
 # Used for create favicon link like this:
