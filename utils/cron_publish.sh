247cdc9bd96cac4cc1e3d598bba913e15e34e48d
Puneeth Chaganti <punchagan@muse-amuse.in>
2013-08-28 10:51:21 +0530

    Update nikola each time cron job runs.

--- utils/cron_publish.sh
+++ utils/cron_publish.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Update nikola
+pushd $HOME/packages/nikola
+git reset --hard
+git checkout master
+git pull origin master
+popd
+
+# Publish site
 source "/home/ec2-user/.virtualenvs/ultimate-sport/bin/activate"
 prefix=`dirname "$0"`
 pushd $prefix
