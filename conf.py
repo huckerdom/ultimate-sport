bc2f1b5837e9a485633a0982524ea4132ba802c7
Puneeth Chaganti <punchagan@muse-amuse.in>
2013-08-31 03:52:26 +0530

    CONF: Try google plus comments.

--- conf.py
+++ conf.py
@@ -291,6 +291,7 @@ CONTENT_FOOTER = CONTENT_FOOTER.format(email=BLOG_EMAIL,
 # http://disqus.com, and set DISQUS_FORUM to the short name you selected.
 # If you want to disable comments, set it to False.
 # Default is "nikolademo", used by the demo sites
+COMMENT_SYSTEM = 'googleplus'
 COMMENT_SYSTEM_ID = "ultimatesport"
 
 # Create index.html for story folders?
